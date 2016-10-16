f11ba67585c0f2915f925661eef1d448dc1e5e87
Benjamin Earl <benear72@gmail.com>
2016-10-16 18:03:55 +0000

    tessalation creator is useable now, but still has a lot of bugs

--- Tessellation/script.js
+++ Tessellation/script.js
@@ -1,9 +1,10 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-var SHOW_GRID = true;
-var GRID_THICKNESS = 0.05;
+var SHOW_GRID = false;
+var GRID_THICKNESS = 0.5;
 var CELL_THICKNESS = 1;
+var mainIsSet = false;
 var mainTessal;
 
 var Grid = {
@@ -101,7 +102,7 @@ function Tessellation(width,height,vertShift,horzShift){
     	this.y = Math.floor(-this.height/2);
         for(var xi = this.x; xi < this.x+this.width; xi++){
             for(var yi = this.y; yi < this.y+this.height; yi++){
-                this.cells.push(new Cell(xi,yi));
+                this.addCell(new Cell(xi,yi));
             }
         }
     };
@@ -162,64 +163,156 @@ function Tessellation(width,height,vertShift,horzShift){
     	}
     	return -1;
     };
+    this.getAbstractIndex = function(cell){
+    	var thisMainEquiv = this.getMainEquiv(cell);
+    	for(var i = 0; i < this.cells.length; i++){
+    		var centralEquiv = this.getMainEquiv(this.cells[i]);
+    		if(thisMainEquiv.x == centralEquiv.x &&
+    		   thisMainEquiv.y == centralEquiv.y){
+    		   	return i;
+    		}
+    	}
+    };
+    this.addCell = function(cellToAdd){
+    	if(this.getindex(cellToAdd) == -1){
+    		this.cells.push(cellToAdd);
+    	}
+    }
     this.removeCell = function(cellToRemove){
     	var index = this.getindex(cellToRemove);
     	if(index == -1){return}
     	this.cells.splice(index,1);
     }
-    this.toggle = function(cellToToggle){
-    	var index = this.getindex(cellToToggle)
-    	if(index != -1){
-    		this.cells.splice(index,1);
-    	}
-    	else{
-    		this.cells.push(cellToToggle);
-    		console.log(Math.abs(cellToToggle.y - this.y));
+    this.isInMain = function(cell){
+    	var column = Math.floor(((cell.x-this.x) - (Math.floor((cell.y-this.y)/this.height)*this.horzShift))/this.width);
+    	var row = Math.floor(((cell.y-this.y) - (Math.floor((cell.x-this.x)/this.width)*this.vertShift))/this.height);
+    	if(column == 0 && row == 0){
+    		return true;
+    	} else {
+    		return false;
     	}
     }
+    this.getMainEquiv = function(cell){
+    	var column = Math.floor(((cell.x-this.x) - (Math.floor((cell.y-this.y)/this.height)*this.horzShift))/this.width);
+    	var row = Math.floor(((cell.y-this.y) - (Math.floor((cell.x-this.x)/this.width)*this.vertShift))/this.height);
+    	var x = cell.x - (column*this.width+row*this.horzShift);
+    	var y = cell.y - (row*this.height+column*this.vertShift);
+    	return new Cell(x,y);
+    }
+    this.toggle = function(cellToToggle){
+    	var index = this.getindex(cellToToggle);
+    	// clicked on a blue cell
+    	if(index != -1){
+    		// remove the blue cell
+    		this.cells.splice(index,1);
+    		// check the 4 cells around it to see where to put it
+    		// if more than one neibor is a valid spot or if it is not
+    		// on the edge then ask where to put it
+    		this.addCell(this.getMainEquiv(cellToToggle));
+    	// clicked on a grey cell
+    	} else {
+    		// remove the associated blue cell
+    		this.cells.splice(this.getAbstractIndex(cellToToggle),1);
+    		// turn the grey cell blue
+    		this.addCell(cellToToggle);
+    	}
+    };
     
 }
 
-/* Mouse Handlers */
 {
     var mouseIsDown = false;
     var mouseDownX = 0;
     var mouseDownY = 0;
+    var topCorner,width,height;
     window.onmousedown = function(Event){
     	mouseIsDown = true;
         mouseDownX = Event.pageX;
         mouseDownY = Event.pageY;
         var cell = new Cell(Math.floor((mouseDownX - Grid.offsetX)/Grid.gridSize),
                 Math.floor((mouseDownY - Grid.offsetY)/Grid.gridSize));
-        mainTessal.toggle(cell);
+        if(mainIsSet){
+        	mainTessal.toggle(cell);
+        } else {
+        	topCorner = cell;
+    	}
         draw();
     };
     window.onmousemove = function(Event){  
-    	if(mouseIsDown){
-    		
+    	var cell = new Cell(Math.floor((Event.pageX - Grid.offsetX)/Grid.gridSize),
+                Math.floor((Event.pageY - Grid.offsetY)/Grid.gridSize));
+    	if(mouseIsDown && !mainIsSet){
+    		width = cell.pxLeft - topCorner.pxLeft;
+    		height = cell.pxTop - topCorner.pxTop;
+    		draw();
+    		ctx.fillRect(Grid.offsetX + Grid.gridSize * topCorner.x,
+    			Grid.offsetY + Grid.gridSize * topCorner.y,width,height);
     	}
     };
     window.onmouseup = function(){
     	mouseIsDown = false;
+    	if(!mainIsSet && (width*height) >= 3){
+    		mainIsSet = true;
+    		mainTessal = new Tessellation(width/Grid.gridSize,height/Grid.gridSize,0,29);
+    		mainTessal.init();
+    		draw();
+    	}
     };
- }
+ } // Mouse Handlers
+
+{
+    window.addEventListener("keydown", function (event) {
+        if (event.defaultPrevented) {
+            return; // Should do nothing if the key event was already consumed.
+            }
+        // 187 +
+        // 189 -
+        
+        //alert(event.keyCode);
+        switch(event.keyCode) {
+            case 187:
+            Grid.zoom("in");
+            break;
+            case 189:
+            Grid.zoom("out");
+            break;
+            case 32: // space
+            break;
+            default:
+            return;
+            }
+        
+        // Consume the event to avoid it being handled twice
+        event.preventDefault();
+        }, true);
+} // Keyboard Handler
 
 function draw() {
     // clear screen
     ctx.clearRect(0,0,canvas.width,canvas.height);
-	Grid.draw();
-	mainTessal.draw();
-	mainTessal.fillBoard();
+	if(SHOW_GRID){Grid.draw();}
+	if(mainIsSet){
+		mainTessal.draw();
+		mainTessal.fillBoard();
+	} else {
+		ctx.font = "20px Arial";
+		ctx.fillText("Click and Drag to make the tessalation the size you want",100,100);
+	}
 }
 
 function init() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     Grid.init();
-    mainTessal = new Tessellation(7,1,0,4);
-    mainTessal.init();
+    mainTessal = new Tessellation(9,3,0,2);
     draw();
 }
 
-window.onresize = init;
+window.onresize = function(){
+	canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    Grid.init();
+    draw();
+}
+
 init();
